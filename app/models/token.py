9dbe04957d731fe57765d3be984ae0474ef0e8b3
Jenna Martin <jennamar828@gmail.com>
2023-11-29 21:00:54 +0000

    headed home commit

--- app/models/token.py
+++ app/models/token.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, DateTime, TIMESTAMP
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped
 import datetime
 
 from app.db.base_class import Base
@@ -10,10 +10,10 @@ class Token(Base):
     def get_future_date():
         return datetime.datetime.utcnow() + datetime.timedelta(days=30)
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = ForeignKey("users.id")
-    token_type = Column(String, default="auth")
-    access_token = Column(String, default="")
-    expires = Column(DateTime, default=get_future_date)
+    id: Mapped[int] = Column(Integer, primary_key=True, index=True)
+    user_id: Mapped[int] = ForeignKey("users.id")
+    token_type: Mapped[str] = Column(String, default="auth")
+    access_token: Mapped[str] = Column(String, default="")
+    expires: Mapped[str] = Column(DateTime, default=get_future_date)
 
     
