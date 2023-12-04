f136e7d119a67a6f64357f1cda551340a0428b70
Jenna Martin <jennamar828@gmail.com>
2023-12-04 13:18:31 +0000

    more endpoints added and connected to api

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -24,3 +24,28 @@ def get_fish() -> Any:
     return {"data":response.json()}
 
 
+@api_router.get("/bugs/")
+def get_bugs() -> Any:
+    api_url = "https://api.nookipedia.com/nh/bugs"
+    headers =  {"X-API-KEY":"2b03414d-aa19-4cf3-a55c-48f2bedd39cd"}
+    response = requests.get(api_url, headers=headers)
+    print (response)
+    return {"data":response.json()}
+
+
+@api_router.get("/deep-sea-creatures/")
+def get_seaCreatures() -> Any:
+    api_url = "https://api.nookipedia.com/nh/sea"
+    headers =  {"X-API-KEY":"2b03414d-aa19-4cf3-a55c-48f2bedd39cd"}
+    response = requests.get(api_url, headers=headers)
+    print (response)
+    return {"data":response.json()}
+
+
+@api_router.get("/events/")
+def get_events() -> Any:
+    api_url = "https://api.nookipedia.com/nh/events"
+    headers =  {"X-API-KEY":"2b03414d-aa19-4cf3-a55c-48f2bedd39cd"}
+    response = requests.get(api_url, headers=headers)
+    print (response)
+    return {"data":response.json()}
