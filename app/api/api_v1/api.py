120a845fdc4eec2fc3e719685c53a2f1f476144f
Jenna Martin <jennamar828@gmail.com>
2023-12-05 13:34:08 +0000

    push up to main

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -9,7 +9,6 @@ from app import schemas
 from app.api.api_v1.endpoints import login, users, utils
 
 
-
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
