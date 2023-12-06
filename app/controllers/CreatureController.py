e1b20927f4d1f1fa4f8747b7884be5f20e985fea
Jenna Martin <jennamar828@gmail.com>
2023-12-06 17:52:17 +0000

    creature controller 'get_by_name' started

--- app/controllers/CreatureController.py
+++ app/controllers/CreatureController.py
@@ -5,19 +5,18 @@ from sqlalchemy.orm import Session
 from app.core.security import get_password_hash, verify_password
 from app.controllers.BaseController import BaseController
 from app.models.creature import Creature
-from app.schemas.creature import CreatureSchema
+from app.schemas.creature import CreatureCreate, CreatureUpdate
 
 
 class CreatureController(BaseController[Creature, CreatureCreate, CreatureUpdate]):
-    def get_by_email(self, db: Session, *, email: str) -> Optional[Creature]:
-        return db.query(Creature).filter(Creature.email == email).first()
+    def get_by_name(self, db: Session, *, name: str) -> Optional[Creature]:
+        return db.query(Creature).filter(Creature.name == name).first()
 
     def create(self, db: Session, *, obj_in: CreatureCreate) -> Creature:
         db_obj = Creature(
-            email=obj_in.email,
-            hashed_password=get_password_hash(obj_in.password),
-            username=obj_in.username,
-            is_superuser=obj_in.is_superuser,
+            name=obj_in.name,
+            location=obj_in.location,
+            creature_type=obj_in.creature_type,
         )
         db.add(db_obj)
         db.commit()
